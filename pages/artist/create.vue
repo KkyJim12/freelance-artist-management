747d0d5cf8bf1819db91d7720eff3e0dc33a9409
Jimmy <jirakarnjim1@gmail.com>
2020-01-21 18:00:01 +0700

    Theme Setting !!
    Theme Setting !!

---
+++ pages/artist/create.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-layout>
+    <v-flex xs12 sm12 md12>
+      <v-container>
+        <v-row>
+          <v-col>
+            <h1>Artist Management Create</h1>
+          </v-col>
+          <v-spacer></v-spacer>
+          <v-col class="text-right">
+            <nuxt-link to="/artist"
+              ><v-btn color="warning">ย้อนกลับ</v-btn></nuxt-link
+            >
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-card class="pa-10">
+        <v-form ref="form" v-model="valid" :lazy-validation="lazy">
+          <v-text-field
+            label="Solo"
+            placeholder="ชื่อศิลปิน"
+            solo
+          ></v-text-field>
+          <v-select :items="items" label="ประเภทศิลปิน" solo></v-select>
+          <v-menu
+            ref="menu"
+            v-model="menu"
+            :close-on-content-click="false"
+            :return-value.sync="date"
+            transition="scale-transition"
+            offset-y
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                v-model="date"
+                label="วันเกิดศิลปิน"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker v-model="date" no-title scrollable>
+              <v-spacer></v-spacer>
+              <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
+              <v-btn text color="primary" @click="$refs.menu.save(date)"
+                >OK</v-btn
+              >
+            </v-date-picker>
+          </v-menu>
+          <v-file-input
+            label="รูปศิลปิน"
+            filled
+            prepend-icon="mdi-camera"
+          ></v-file-input>
+          <v-btn color="success" block>ยืนยัน</v-btn>
+        </v-form>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: ["Foo", "Bar", "Fizz", "Buzz"]
+  })
+};
+</script>
---
+++ pages/artist/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-layout>
+    <v-flex xs12 sm12 md12>
+      <v-container>
+        <v-row>
+          <v-col>
+            <h1>Artist Management</h1>
+          </v-col>
+          <v-spacer></v-spacer>
+          <v-col class="text-right">
+            <nuxt-link to="/artist/create"
+              ><v-btn color="success">สร้าง</v-btn></nuxt-link
+            >
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-simple-table>
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left">รูปภาพ</th>
+              <th class="text-left">ชื่อศิลปิน</th>
+              <th class="text-left">วันเกิด</th>
+              <th class="text-left">ประเภทศิลปิน</th>
+              <th class="text-left">แก้ไข</th>
+              <th class="text-left">ลบ</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in desserts" :key="item.name">
+              <td>{{ item.name }}</td>
+              <td>{{ item.calories }}</td>
+              <td></td>
+              <td></td>
+              <td><v-btn color="warning">แก้ไข</v-btn></td>
+              <td><v-btn color="error">ลบ</v-btn></td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      desserts: [
+        {
+          name: "The Toy",
+          calories: "The Toy"
+        },
+        {
+          name: "Black Pink",
+          calories: "Black Pink"
+        },
+        {
+          name: "BTS",
+          calories: "BTS"
+        }
+      ]
+    };
+  }
+};
+</script>
